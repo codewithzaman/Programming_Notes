4481dee43d1711772519b9294c9cc1fb99553fb0
codewithzaman <naderzaman50@gmail.com>
2023-09-23 12:28:02 +0500

    useEffectHook

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-import UseStateHook from './UseStateHook';
+// import UseStateHook from './UseStateHook';
+import UseEffectHook from "./UseEffectHook"
+import { useState } from 'react';
 
 function App() {
+  const [state, setState] = useState(true)
   return (
     <div className="App">
-     <UseStateHook/>
+     {/* <UseStateHook/> */}
+     <button onClick={(e)=> setState(!state)}>Toggle</button>
+     {state ? <UseEffectHook/> : ""}
     </div>
   );
 }
--- react-hooks/src/UseEffectHook.js
+++ react-hooks/src/UseEffectHook.js
@@ -1,11 +1,35 @@
 // UseEffectHook :
-// By Using this hook, you tell the react that your components needs to do something after render. raect will remember the function u passed . 
+// By Using this hook, you tell the react that your components needs to do something after render. raect will remember the function u passed . This is a hook in react that is used to perform side effects. side effects are the results of state changing . we have some states in our application its value change from one state to an other . 
 
 
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
+
 
 export default function Demo () {
 const [count, setCount] = useState(0);
+// useEffect(()=>{
+// // that code we want to run 
+// console.log("the count is :",count)
+// //optional return function 
+// return ()=>{
+//     console.log("i am cleaned up!!");
+// }
+// },[count]) // The dependency Array is used to tell the useEffect what to do 
+useEffect(() => {
+  console.log("Counter Mounted");
+
+  return () => {
+    console.log("Counter Unmounted");
+  }
+}, [])// When  mounting then we will left this array empty. 
+useEffect(() => {
+  console.log("User Updated Count",count);
+
+  return () => {
+    console.log("Counter UseEffect Return",count);
+  }
+}, [count]) // when updating then  we will write the value in the array . 
+
 return (
 <div className='tutorial'>
 <h1>Count: {count}</h1>
