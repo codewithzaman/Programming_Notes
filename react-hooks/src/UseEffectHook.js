df9ae1b49f00bba667a9acda9017af780f0ff085
codewithzaman <naderzaman50@gmail.com>
2023-09-23 11:00:42 +0500

    Update UseEffectHook.js

--- react-hooks/src/UseEffectHook.js
+++ react-hooks/src/UseEffectHook.js
@@ -1,2 +1,20 @@
 // UseEffectHook :
-// By Using this hook, you tell the react that your components needs to do something after render 
+// By Using this hook, you tell the react that your components needs to do something after render. raect will remember the function u passed . 
+
+
+import { useState } from 'react';
+
+export default function Demo () {
+const [count, setCount] = useState(0);
+return (
+<div className='tutorial'>
+<h1>Count: {count}</h1>
+<button onClick={() => setCount (count - 1)}>
+Decrement
+</button>
+<button onClick={() => setCount (count + 1)}>
+Increment
+</button>
+</div>
+);
+}
