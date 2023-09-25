2edadf1591ac9e70648fe4a7a545c8d0f3ceac2f
codewithzaman <naderzaman50@gmail.com>
2023-09-25 18:52:19 +0500

    Update script.js
    Constants and Variables

--- JavaScript_Basics_Sheriyan/script.js
+++ JavaScript_Basics_Sheriyan/script.js
@@ -3,3 +3,16 @@
 // KeyWord: jis chez ka js mn koe matlab ho usko keyword kehtay hn jesay k for , let , each , var , const etc.
 
 // 2) Var Const and Let
+// Variables and constants 
+// Variable : code mn koe data store krnay k  leay jo use hota hy ussay variable kehtay hn jis ke value ko change b kr sktay hn .
+// Constants : code mn koe data store krnay k  leay jo use hota hy ussay constant kehtay hn jis ke value ko change nhi kr sktay hn .
+
+// var dulha = "lab";
+// var dulhan = "laby";
+// dulha = "harry"
+// console.log(dulha + " " + "weds" + " " + dulhan);
+
+const dulha = "lab";
+const dulhan = "laby";
+dulha = "harry" // we can not change the value of dulha as we use const
+console.log(dulha + " " + "weds" + " " + dulhan);
