59f2d60be921be284df0ed6ea92e6225c3177786
codewithzaman <naderzaman50@gmail.com>
2023-09-25 17:44:21 +0500

    Update script.js

--- JavaScript_Basics_Sheriyan/script.js
+++ JavaScript_Basics_Sheriyan/script.js
@@ -1,4 +1,5 @@
-// Js 
-// Word and Keyword 
-// Var Const Let
-// 
+// 1) Word and KeyworD
+// Word : jis chez ka js mn koe matlab na ho usko word kehtay hn jesay k chacha , good , man 
+// KeyWord: jis chez ka js mn koe matlab ho usko keyword kehtay hn jesay k for , let , each , var , const etc.
+
+// 2) Var Const and Let
